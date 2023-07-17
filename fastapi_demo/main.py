580cb5d708200d1c71624cd01e4a0b0ac1672674
G0erman <goerman.rafi@gmail.com>
2023-07-16 19:46:23 -0500

    Using async and models

--- fastapi_demo/main.py
+++ fastapi_demo/main.py
@@ -1,17 +1,33 @@
 """Original Example from https://fastapi.tiangolo.com/."""
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
 
+class Item(BaseModel):
+    """Define the model of the input."""
+
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
 @app.get("/")
-def read_root():
+async def read_root():
     """Returns a dictionary."""
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, query: Union[str, None] = None):
+async def read_item(item_id: int, query: Union[str, None] = None):
     """Automatic valiation from type integer"""
     return {"item_id": item_id, "query": query}
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    """The item to updated must have the BaseModel."""
+    return {"item_name": item.name, "item_id": item_id}
